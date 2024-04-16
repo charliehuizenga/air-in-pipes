e0a62922ef648bbacce938003cdb2f0cc6e44b32
Mario Gonzalez <mario.gonzalez@berkeley.edu>
2024-04-15 21:51:29 -0700

    made entire homepage path lowercase

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '',
-    assetPrefix: '',
     typescript: {
       // !! WARN !!
       // Dangerously allow production builds to successfully complete even if
