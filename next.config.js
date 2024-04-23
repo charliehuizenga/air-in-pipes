c8ae21e52dd23f2876d5e53abc034da1119de9df
Mario Gonzalez <mario.gonzalez@berkeley.edu>
2024-04-23 13:03:37 -0700

    merge

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: 'standalone',
     typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
       ignoreBuildErrors: false,
     },
   };
