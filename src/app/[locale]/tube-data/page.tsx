8afdbad80ba754cfc47e191b43f7a3fa6273adfb
steffitan23 <steffitlw@berkeley.edu>
2025-02-20 12:04:19 -0800

    Fix duplicate cost values

--- src/app/[locale]/tube-data/page.tsx
+++ src/app/[locale]/tube-data/page.tsx
@@ -232,7 +232,7 @@ export default function TubeData() {
           <input
             type="number"
             placeholder="Id"
-            value={newPipeCost}
+            value={newPipeId}
             onChange={(e) => setNewPipeId(e.target.value)}
             className="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6"
           />
@@ -350,7 +350,6 @@ export default function TubeData() {
                 onClick={() => {
                   calculate();
                   saveProject();
-                  console.log(project);
                   router.push(`/${locale}/report?uuid=${project.uuid}`);
                 }}
                 className="px-4 py-2 bg-green-500 text-white rounded-md shadow-sm hover:bg-green-600"
