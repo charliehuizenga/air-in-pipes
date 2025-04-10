c346bf509448b03b8e4dc6419fb8974ef86b1089
steffitan23 <steffitlw@berkeley.edu>
2025-04-10 14:00:21 -0700

    Fix bug with default notes value and remove templates

--- src/app/[locale]/components/details.tsx
+++ src/app/[locale]/components/details.tsx
@@ -3,34 +3,18 @@
 import { setProject } from "../redux/project-slice";
 import { useDispatch, useSelector } from "react-redux";
 import { useTranslations } from "next-intl";
-import { ProjectState } from "../redux/store";
-import { useState } from "react";
-import example0 from "../../../../examples/Example0.json";
-import example1 from "../../../../examples/Example1.json";
-import example2 from "../../../../examples/Example2.json";
-import example3 from "../../../../examples/Example3.json";
-import example6 from "../../../../examples/Example6.json";
-import example7 from "../../../../examples/Example7.json";
 
-const manualExampleFiles = [
-  { name: "Example 0", content: example0 },
-  { name: "Example 1", content: example1 },
-  { name: "Example 2", content: example2 },
-  { name: "Example 3", content: example3 },
-  { name: "Example 6", content: example6 },
-  { name: "Example 7", content: example7 },
-];
-
-export default function Details() {
+export default function Details({ project }) {
   const dispatch = useDispatch();
   const t = useTranslations("principal");
-  const project = useSelector((state: ProjectState) => state.project);
+  const today = new Date().toISOString().split("T")[0];
 
   const handleChange = (
     e: React.ChangeEvent<
       HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
     >
   ) => {
+    console.log(project);
     const { name, value } = e.target;
 
     dispatch(
@@ -39,35 +23,6 @@ export default function Details() {
         [name]: name === "qmax" || name === "qmin" ? parseFloat(value) : value,
       })
     );
-  };
-
-  const [selectedExampleName, setSelectedExampleName] = useState("");
-
-  const handleExampleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    if (event.target.value == "") {
-      setSelectedExampleName("");
-      return;
-    }
-
-    const newFileName = event.target.value;
-    const exampleFile = manualExampleFiles.find(
-      (file) => file.name === newFileName
-    );
-    if (exampleFile) {
-      dispatch(
-        setProject({
-          ...exampleFile.content,
-          uuid: project.uuid || exampleFile.content.uuid,
-          org_id: project.org_id,
-          user_id: project.user_id,
-        })
-      );
-      
-
-      setSelectedExampleName(newFileName);
-    } else {
-      console.error("Example file not found:", newFileName);
-    }
   };
 
   return (
@@ -95,30 +50,7 @@ export default function Details() {
                     />
                   </div>
                 </div>
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="template"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    {t("template")}
-                  </label>
-                  <div className="mt-2">
-                    <select
-                      name="project"
-                      id="project"
-                      value={project.template || ""}
-                      onChange={handleExampleChange}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6"
-                    >
-                      <option value="">None (Optional)</option>
-                      {manualExampleFiles.map((file) => (
-                        <option key={file.name} value={file.name}>
-                          {file.name}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                </div>
+
                 <div className="sm:col-span-3">
                   <label
                     htmlFor="designer-name"
@@ -166,10 +98,10 @@ export default function Details() {
                   </label>
                   <div className="mt-2">
                     <input
-                      type="text"
+                      type="date"
                       name="date"
                       id="date"
-                      value={project.date || ""}
+                      value={project.date || today}
                       onChange={handleChange}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6"
                     />
@@ -187,7 +119,7 @@ export default function Details() {
                     <textarea
                       id="notes"
                       name="notes"
-                      defaultValue={project.notes || ""}
+                      value={project.notes || ""}
                       rows={3}
                       onChange={handleChange}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6"
@@ -198,10 +130,9 @@ export default function Details() {
             </div>
 
             <div className="border-b border-gray-900/10 pb-12">
-              <h2 className="text-base font-semibold leading-7 text-gray-900">
+              <h2 className="text-base font-semibold leading-6 text-gray-900">
                 {t("design-options")}
               </h2>
-              <p className="mt-1 text-sm leading-6 text-gray-600"></p>
 
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div className="sm:col-span-3">
@@ -217,7 +148,7 @@ export default function Details() {
                       name="qmin"
                       value={project.qmin || ""}
                       onChange={handleChange}
-                      id="minimum-flow  "
+                      id="minimum-flow"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6"
                     />
                   </div>
@@ -242,6 +173,7 @@ export default function Details() {
                   </div>
                 </div>
               </div>
+
               <div className="mt-10 space-y-10">
                 <fieldset>
                   <legend className="text-sm font-semibold leading-6 text-gray-900">
@@ -258,7 +190,7 @@ export default function Details() {
                         type="radio"
                         onChange={handleChange}
                         value="auto"
-                        checked={project.airvalve_selection == "auto"}
+                        checked={project.airvalve_selection === "auto"}
                         className="h-4 w-4 border-gray-300 text-sky-500 focus:ring-sky-500"
                       />
                       <label
@@ -275,7 +207,7 @@ export default function Details() {
                         type="radio"
                         onChange={handleChange}
                         value="manual"
-                        checked={project.airvalve_selection == "manual"}
+                        checked={project.airvalve_selection === "manual"}
                         className="h-4 w-4 border-gray-300 text-sky-500 focus:ring-sky-500"
                       />
                       <label
@@ -295,4 +227,3 @@ export default function Details() {
     </main>
   );
 }
-
